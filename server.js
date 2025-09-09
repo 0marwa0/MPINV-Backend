4de47c9dd83673b74a783cc30216b510e83cb876
0marwa0 <marwajawad19@gmail.com>
2025-09-09 13:42:47 +0400

    adding port number

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.get("/api/leads", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
