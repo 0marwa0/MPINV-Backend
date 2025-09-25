e980f153b629d120b0bc533522d3d2bc55306215
0marwa0 <marwajawad19@gmail.com>
2025-09-25 20:13:47 +0400

    fix deploy issue

--- db_connection.js
+++ db_connection.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://MPINV:0000@cluster0.wh2mnc2.mongodb.net/test?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const mongoURI =
+      process.env.MONGODB_URI ||
+      "mongodb+srv://MPINV:0000@cluster0.wh2mnc2.mongodb.net/test?retryWrites=true&w=majority";
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ Mongo error:", err.message);
